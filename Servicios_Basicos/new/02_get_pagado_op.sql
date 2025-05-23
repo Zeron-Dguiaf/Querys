d8556d010f9d17571998cac20359e923c920a912
zeron <161514797+zeron-team@users.noreply.github.com>
2025-05-23 19:06:40 +0200

    Update 02_get_pagado_op.sql

--- Servicios_Basicos/new/02_get_pagado_op.sql
+++ Servicios_Basicos/new/02_get_pagado_op.sql
@@ -1,1 +1,35 @@
+-- GET PAGADO OP
+CREATE OR REPLACE FUNCTION SLU.get_pagado_op(
+--
+p_aa_formulario IN slu.tformulario.aa_formulario%TYPE,
+p_t_formulario IN slu.tformulario.t_formulario%TYPE,
+p_o_formulario IN slu.tformulario.o_formulario%TYPE,
+p_t_pago IN slu.tpago.t_pago%TYPE
+)
+RETURN NUMBER IS
+--
+l_return number;
 
+BEGIN
+--
+BEGIN
+--
+SELECT sum(ia_pago) i_pago
+INTO l_return
+FROM slu.tpago p
+WHERE p.aa_op=p_aa_formulario
+AND p.t_op=p_t_formulario
+AND p.o_op=p_o_formulario
+AND p.t_pago=p_t_pago
+AND p.e_pago not in ('X','I')
+--AND p.c_retencion=7
+;
+--
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+l_return := 0;
+END;
+--
+RETURN NVL(l_return,0);
+--
+END;
